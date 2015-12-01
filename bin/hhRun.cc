d76acf357e7c41ac8c957febf9bac76289b8809f
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2015-12-01 13:01:20 +0100

    Add missing files

---
+++ bin/hhRun.cc
@@ -0,0 +1,32 @@
+#include "../interface/HHbbggAnalyzer.h"
+#include "FWCore/PythonParameterSet/interface/MakeParameterSets.h"
+#include "PhysicsTools/UtilAlgos/interface/FWLiteAnalyzerWrapper.h"
+
+typedef fwlite::AnalyzerWrapper<HHbbggAnalyzer> WrappedFWLiteHHbbggAnalyzer;
+
+int main(int argc, char* argv[]) 
+{
+  // load framework libraries
+  gSystem->Load( "libFWCoreFWLite" );
+  AutoLibraryLoader::enable();
+  
+  gSystem->Load( "libDataFormatsFWLite.so" );
+  gSystem->Load( "libDataFormatsPatCandidates.so" );
+  gSystem->Load( "libflashggDataFormats.so" );
+
+  for (int a=0; a<argc; a++){
+    std::cout<<a<<"  "<<argv[a]<<std::endl;
+  }
+  if ( argc < 2 ) {
+    std::cout << "Usage : " << argv[0] << " [parameters.py]" << std::endl;
+    return 0;
+  }
+
+  PythonProcessDesc builder( argv[1], argc, argv );
+  WrappedFWLiteHHbbggAnalyzer ana( *( builder.processDesc()->getProcessPSet() ), std::string( "HHbbggAnalyzer" ), std::string( "test" ) );
+
+  ana.beginJob();
+  ana.analyze();
+  ana.endJob();
+  return 0;
+}
